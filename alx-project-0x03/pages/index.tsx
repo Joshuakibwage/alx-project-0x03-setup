f412729438ee892a57d5084d9613b5703cb314d4
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-07-27 21:24:35 +0300

    feat: implementing a shared layout

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/layouts/Layout';
+
+
+const Home: React.FC = () => {
+  return (
+    <main className="min-h-screen">
+      <Layout />
+    </main>
+  )
+}
+
+
+
+export default Home;
